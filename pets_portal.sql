c9a1f575ff2d98f4a9468d10d2db61009a5da9dd
Safalya Kumbhare <safalyakumbhare@gmail.com>
2024-10-17 21:31:22 +0530

    first commit

---
+++ pets_portal.sql
@@ -0,0 +1,63 @@
+-- Database name  `pets`
+
+CREATE DATABASE pets;
+
+USE pets;
+
+-- Create the `roles` table
+
+CREATE TABLE roles (
+    role_id INT AUTO_INCREMENT PRIMARY KEY,
+    role VARCHAR(255) NOT NULL UNIQUE,
+    status VARCHAR(255) NOT NULL DEFAULT 'active',
+);
+
+INSERT INTO roles (role) VALUES
+('admin'),
+('user');
+
+-- Create the `users` table
+
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(10) NOT NULL UNIQUE,
+    address VARCHAR(255) NOT NULL,
+    gender VARCHAR(15) NOT NULL,
+    dob DATE NOT NULL,
+    role_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    status VARCHAR(255) NOT NULL DEFAULT 'active',
+    FOREIGN KEY (role_id) REFERENCES roles(role_id)
+);
+
+
+-- Insert admin in users 
+
+INSERT INTO users (username, email, password, phone, address, gender, dob, role_id) VALUES
+('Safalya Kumbhare','safalyakumbhare@gmail.com','123456','9874563215','Trimurti Nagar, Nagpur','male','1999-10-02',1);
+
+
+-- create pets table 
+
+    CREATE TABLE pets (
+        pet_id INT PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(255) NOT NULL,
+        type VARCHAR(255) NOT NULL,
+        breed VARCHAR(255) NOT NULL,
+        dob DATE NOT NULL,
+        gender VARCHAR(15) NOT NULL,
+        color VARCHAR(255) NOT NULL,
+        weight VARCHAR(10) NOT NULL,
+        image VARCHAR(100) NOT NULL,
+        medical TEXT NOT NULL,
+        note TEXT,
+        user_id INT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        status VARCHAR(255) NOT NULL DEFAULT 'active',
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    );
---
+++ user_registration.php
@@ -0,0 +1,148 @@
+<?php
+include("common/connection.php");
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $confirmpassword = $_POST['confirmpassword'];
+    $dob = $_POST['dob'];
+    $phone = $_POST['phone'];
+    $address = $_POST['address'];
+    $gender = $_POST['gender'];
+    $filename = $_FILES["imageupload"]["name"];
+    $tempname = $_FILES["imageupload"]["tmp_name"];
+
+    if ($_POST['password'] !== $confirmpassword) {
+        echo "<script>alert('Passwords do not match');
+        window.location.href='user_registration.php'</script>";
+    } else {
+        $check = "SELECT * FROM users WHERE username ='$name'";
+        $result = mysqli_query($conn, $check);
+
+        if (mysqli_num_rows($result) > 0) {
+            echo "<script>alert('User Already Registered');
+            window.location.href='user_registration.php';</script>";
+        } else {
+            $folder = "assets/images/" . basename($filename);
+
+            $sql = "INSERT INTO users(username, email, password, phone, address, gender, dob, role_id, profile) 
+                    VALUES ('$name', '$email', '$password', '$phone', '$address', '$gender', '$dob', 2, '$filename');";
+
+            $insert = mysqli_query($conn, $sql);
+
+            if ($insert) {
+                if (move_uploaded_file($tempname, $folder)) {
+                    echo "<script>alert('User Registered Successfully');
+                    window.location.href='login.php';</script>";
+                } else {
+                    echo "<script>alert('Failed to upload image');
+                    window.location.href='user_registration.php';</script>";
+                }
+            } else {
+                echo "<script>alert('Error occurred while registering user');
+                window.location.href='user_registration.php';</script>";
+            }
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>User Registration</title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="assets/fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="assets/css/user_registration_css/style.css">
+</head>
+
+<body>
+
+    <div class="main">
+        <div class="container">
+            <div class="signup-content">
+                <div class="signup-img">
+                    <img src="assets/images/user_registration.jpg" alt="">
+                </div>
+                <div class="signup-form">
+                    <form method="POST" class="register-form" id="register-form" enctype="multipart/form-data">
+                        <h2>User Registration Form</h2>
+
+                        <div class="form-group">
+                            <label for="name">Username :</label>
+                            <input type="text" name="name" id="name" placeholder="Enter your Name" required />
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email Address :</label>
+                            <input type="email" name="email" placeholder="Enter your email" id="email" required />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="phone">Phone Number :</label>
+                            <input type="phone" name="phone" placeholder="Enter your phone number" id="phone"
+                                required />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="address">Address :</label>
+                            <!-- <input type="text" name="address"  id="address" required/> -->
+                            <textarea name="address" id="address" placeholder="Enter your address" required></textarea>
+                        </div>
+                        <div class="form-radio">
+                            <label for="gender" class="radio-label">Gender :</label>
+                            <div class="form-radio-item">
+                                <input type="radio" name="gender" id="male" value="male">
+                                <label for="male">Male</label>
+                                <span class="check"></span>
+                            </div>
+                            <div class="form-radio-item">
+                                <input type="radio" name="gender" id="female" value="female">
+                                <label for="female">Female</label>
+                                <span class="check"></span>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="dob">DOB :</label>
+                            <input type="date" name="dob" id="dob" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="pincode">Pincode :</label>
+                            <input type="text" name="pincode" id="pincode">
+                        </div>
+                        <div class="form-group">
+                            <label for="imageupload">Profile Photo :</label>
+                            <input type="file" name="imageupload" class="form-control" accept="image/*" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password :</label>
+                            <input type="password" name="password" id="password" />
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Confirm Password :</label>
+                            <input type="confirmpassword" name="confirmpassword" id="confirmpassword" />
+                        </div>
+                        <div class="form-submit">
+                            <input type="submit" value="Reset All" class="submit" name="reset" id="reset" />
+                            <input type="submit" value="Register" class="submit" name="submit" id="submit" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- JS -->
+    <script src="assets/js/user_registration_js/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/js/user_registration_js/main.js"></script>
+</body>
+
+</html>
